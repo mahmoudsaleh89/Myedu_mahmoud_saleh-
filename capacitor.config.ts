f1d548b759f42a76c2fecfd6fdd396f0b3f4703d
Mahmoud Saleh <m.saleh@atbpay.ae>
2025-04-11 11:45:40 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Myedu',
+  webDir: 'www'
+};
+
+export default config;
